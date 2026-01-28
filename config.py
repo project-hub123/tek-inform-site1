7d3b9662c3192ea0f0cb5c8571066c0cfb493907
Anna1983-lang <rigikanuta@gmail.com>
2026-01-28 13:27:34 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "tek-inform-secret-key")
+    SECRET_KEY = os.environ.get("SECRET_KEY", "tek-inform-2026")
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
