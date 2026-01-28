0a417fff41842013bc124c68caa49fcecf40be8d
Anna1983-lang <rigikanuta@gmail.com>
2026-01-28 13:04:15 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,23 +1,17 @@
 import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "tek-inform-secret-key")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # КРИТИЧЕСКИ ВАЖНО
-    # Flask-WTF установлен → CSRF включён по умолчанию → POST = 500
+    # Flask-WTF установлен → отключаем CSRF
     WTF_CSRF_ENABLED = False
 
 
 class DevelopmentConfig(Config):
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "site.db")
+    pass
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL",
-        "sqlite:///" + os.path.join(BASE_DIR, "site.db")
-    )
+    pass
