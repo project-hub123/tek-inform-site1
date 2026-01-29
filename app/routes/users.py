78bba666647782439698ca24d0529695bc25313e
Anna1983-lang <rigikanuta@gmail.com>
2026-01-29 09:43:46 +0200

    1

--- app/routes/users.py
+++ app/routes/users.py
@@ -1,14 +1,13 @@
 from flask import Blueprint, render_template
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 users_bp = Blueprint(
     "users",
     __name__,
-    url_prefix="/users",
     template_folder="../templates"
 )
 
 @users_bp.route("/profile")
 @login_required
 def profile():
-    return render_template("profile.html")
+    return render_template("profile.html", user=current_user)
