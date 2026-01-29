cd298da84157b7ef2720df7d0697f7330d17093b
Anna1983-lang <rigikanuta@gmail.com>
2026-01-29 09:16:36 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,18 +2,23 @@ import os
 
 
 class Config:
-    
     SECRET_KEY = os.environ["SECRET_KEY"]
 
     SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    
+    # --- ВАЖНО ДЛЯ RENDER / HTTPS ---
+    SESSION_COOKIE_SECURE = True
+    SESSION_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_SAMESITE = "Lax"
+
+    # CSRF 
     WTF_CSRF_ENABLED = False
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    SESSION_COOKIE_SECURE = False  # локально без https
 
 
 class ProductionConfig(Config):
